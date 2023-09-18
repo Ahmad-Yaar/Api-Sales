720820b12df4ef61d94b10e43467ddc3b2a7f63a
Ahmad-Yaar <ahmadyaar0306@gmail.com>
2023-09-18 06:40:13 +0500

    App Name Change

--- src/screens/SyncData.tsx
+++ src/screens/SyncData.tsx
@@ -1,39 +1,43 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { FlatList, ActivityIndicator } from "react-native";
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SyncData = () => {
 
-  const [data, setData] = useState()
-     const handleFetchData = () => {
-         fetch('https://api.apithreesixty.com/api/General/SyncDownload', {
-             method: 'GET',
- 
-         })
-             .then((response) => {
-                 if (!response.ok) {
-                     throw new Error('Network response was not ok');
-                 }
-                 return response.json();
-             })
-             .then((responseData) => {
-                 setData(responseData);
-             })
-             .catch((error) => {
-                 console.error(error);
-             });
-     }
-     useEffect(() => {
-         handleFetchData();
-     }, [])
- 
+    const [data, setData] = useState()
+    const handleFetchData = () => {
+        fetch('https://api.apithreesixty.com/api/General/SyncDownload', {
+            method: 'GET',
 
-    
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((responseData) => {
+                setData(responseData);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+    useEffect(() => {
+        handleFetchData();
+    }, [])
+
+
+
 
     return (
-        <View>
-            <Text>{JSON.stringify(data,null,2)}</Text>
-        </View>
+        <ScrollView>
+            <View>
+
+                <Text>{JSON.stringify(data, null, 2)}</Text>
+            </View>
+        </ScrollView>
     )
 }
 
