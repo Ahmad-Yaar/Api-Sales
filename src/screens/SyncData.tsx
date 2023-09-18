8474cf2c93f236a4da7b56d66a686264bdcda976
Ahmad-Yaar <ahmadyaar0306@gmail.com>
2023-09-18 05:49:05 +0500

    sync

--- src/screens/SyncData.tsx
+++ src/screens/SyncData.tsx
@@ -1,39 +1,38 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { FlatList } from 'react-native-reanimated/lib/typescript/Animated'
+import { FlatList, ActivityIndicator } from "react-native";
 
 const SyncData = () => {
 
-    const [data, setData] = useState()
-    const handleFetchData = () => {
-        fetch('https://api.apithreesixty.com/api/General/SyncDownload', {
-            method: 'GET',
-
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((responseData) => {
-                setData(responseData);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-    useEffect(() => {
-        handleFetchData();
-    }, [])
-
-
-
+  const [data, setData] = useState()
+     const handleFetchData = () => {
+         fetch('https://api.apithreesixty.com/api/General/SyncDownload', {
+             method: 'GET',
+ 
+         })
+             .then((response) => {
+                 if (!response.ok) {
+                     throw new Error('Network response was not ok');
+                 }
+                 return response.json();
+             })
+             .then((responseData) => {
+                 setData(responseData);
+             })
+             .catch((error) => {
+                 console.error(error);
+             });
+     }
+     useEffect(() => {
+         handleFetchData();
+     }, [])
+ 
 
     
+
     return (
         <View>
-            <Text> {JSON.stringify(data,null,2)} </Text>
+            <Text>{JSON.stringify(data,null,2)}</Text>
         </View>
     )
 }
